1cb4bc8b98691c914370cf9b814eafe77fea17d0
Normando115 <49627247+Normando115@users.noreply.github.com>
2022-03-07 16:35:08 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,21 @@
+let blank = [ {
+name: "apple",
+color: "Hershey Bar"
+} {
+name: "Horse",
+color: "Human"
+} {
+name: "Black",
+color: "Poop"
+}];
+
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(1535, 860);
+createButton("Start").mousePressed(startSketch);
+console.log(blank[0]);
+
 }
+
 
 function draw() {
   background(220);
