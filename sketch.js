7e5d792558dace6ed3e2f84e6225e954e96852bc
Normando115 <49627247+Normando115@users.noreply.github.com>
2022-03-13 20:32:15 -0700

    Randomizer
    Food

--- sketch.js
+++ sketch.js
@@ -1,22 +1,58 @@
-let blank = [ {
-name: "apple",
-color: "Hershey Bar"
-} {
-name: "Horse",
-color: "Human"
-} {
-name: "Black",
-color: "Poop"
-}];
+let Breakfast = ['Cereal', 'PBJ', 'Eggs and Bacon', 'Ramen', 'Try again', 'ask someone else', 'eat out', 'make something new'];
+let Lunch = ['Ramen', 'Fried Rice', 'Spaghetti', 'Curry', 'make something new', 'Try again', 'eat out', 'Ubereats', 'ask someone else'];
+let Dinner = ['Ramen', 'Fried Rice', 'Spaghetti', 'Curry', 'make something new', 'Try again', 'eat out', 'Ubereats', 'ask someone else'];
 
-function setup() {
-  createCanvas(1535, 860);
-createButton("Start").mousePressed(startSketch);
-console.log(blank[0]);
+let button1;
+let button2;
+let button3;
 
+function setup(){
+  createCanvas (400,400);
+background(255);
+        let button1 = createButton("Breakfast");
+  button1.mousePressed(breakfastButton);
+  button1.size(130,80);
+  button1.position(10,30);
+  button1.style("font-family", "Comic Sans MS");
+  button1.style("font-size", "24px");
+        let button2 = createButton("Lunch");
+  button2.mousePressed(lunchButton);
+  button2.size(130,80);
+  button2.position(10,140);
+  button2.style("font-family", "Comic Sans MS");
+  button2.style("font-size", "24px");
+        let button3 = createButton("Dinner");
+  button3.mousePressed(dinnerButton);
+  button3.size(130,80);
+  button3.position(10,250);
+  button3.style("font-family", "Comic Sans MS");
+  button3.style("font-size", "24px");
+  
+        let button4 = createButton("Reset");
+  button4.mousePressed(resetButton);
+  button4.position(340,370);
+  button4.style('color:blue');
+
+    brk = int(random(Breakfast.length));
+      lch = int(random(Lunch.length));
+      dnr = int(random(Dinner.length));
+  w1 = Breakfast[brk];
+  w2 = Lunch[lch];
+  w3 = Dinner[dnr];
+  textSize(16);
+  
 }
 
-
-function draw() {
-  background(220);
+function breakfastButton(){
+  text(w1, width*0.45, height*0.2);
+  
+}function lunchButton(){
+   text(w2, width*0.45, height*0.46);
+  
+}function dinnerButton(){
+   text(w3, width*0.45, height*0.72);
 }
+
+function resetButton() {
+  setup();
+  }
