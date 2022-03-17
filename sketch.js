078073a9a5627e22e6f475f254ffb6b5c0930433
Normando115 <49627247+Normando115@users.noreply.github.com>
2022-03-16 17:39:07 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,35 +2,60 @@ let rotVar = 0;
 let spinnerTime;
 let frameCheck;
 let result;
+//image array
+let brk = [];
+let lch = [];
+let dnr = []
 
-let img = [];
 
 let Breakfast = ['Cereal', 'PBJ', 'Eggs and Bacon', 'Ramen', 'Try again', 'eat out'];
 let Lunch = ['Ramen', 'Fried Rice', 'Spaghetti', 'Curry', 'make something new', 'Try again', 'eat out', 'Ubereats', 'ask someone else'];
 let Dinner = ['Ramen', 'Fried Rice', 'Spaghetti', 'Curry', 'make something new', 'Try again', 'eat out', 'Ubereats', 'ask someone else'];
 
 function preload() {
-  for (var i = 0; i < 7; i++) {
-    img[i] = loadImage("Assets/Food" + i + ".jpg");
+  for (var i = 0; i < 8; i++) {
+    brk[i] = loadImage("Assets/Food" + i + ".jpg");
   }
+ //   for (var i = 0; i < 7; i++) {
+   // lch[i] = loadImage("Assets/Food1" + i + ".jpg");
+  //}
+   // for (var i = 0; i < 7; i++) {
+    //dnr[i] = loadImage("Assets/Food2" + i + ".jpg");
+ // }
+
+
  }
 
 
 function setup() {
   createCanvas(585,585);
-  image(img[6], 40, 10, 110, 110);
+  image(brk[6], 40, 10, 110, 110);
   angleMode(DEGREES);
   frameRate(60);
+//Buttons
   buttonSpin = createButton('Spin!');
   buttonSpin.parent('button-holder');
-    buttonSpin.size(width/7,height/13);
+  buttonSpin.size(width/7,height/13);
   buttonSpin.position(width/1.25,height/1.1);
-  buttonSpin.mousePressed(buttonPressed);
-frameRate(30);
-      
+  buttonSpin.mousePressed(buttonPressed4);
+
+    buttonBreakfast = createButton('Breakfast');
+  buttonBreakfast.parent('button-holder');
+    buttonBreakfast.size(width/7,height/13);
+  buttonBreakfast.position(width/6,height/1.1);
+  buttonBreakfast.mousePressed(buttonPressed1);
+      buttonLunch = createButton('Lunch');
+  buttonLunch.parent('button-holder');
+    buttonLunch.size(width/7,height/13);
+  buttonLunch.position(width/3,height/1.1);
+  buttonLunch.mousePressed(buttonPressed2);
+      buttonDinner = createButton('Dinner');
+  buttonDinner.parent('button-holder');
+    buttonDinner.size(width/7,height/13);
+  buttonDinner.position(width/2,height/1.1);
+  buttonDinner.mousePressed(buttonPressed3);
 
 }
-
 function draw() {
   //Spinner
   push();
@@ -47,10 +72,10 @@ function draw() {
   arc(0,0, width*0.75, height*0.75, 180 , 240);
   fill(222, 9, 9);
   arc(0,0, width*0.75, height*0.75, 240 , 300);
-  fill(242, 234, 68);
+  fill(234, 247, 54);
   arc(0,0, width*0.75, height*0.75, 300 , 360);
   pop();
-  
+
   push();
   fill(50,60,20);
   noStroke();
@@ -60,53 +85,78 @@ function draw() {
   rect(0, 0, 20, 100);
   triangle(0,-60,-20,-40,20, -40)
   pop();
-  fill(120,52,33);
-  noStroke();
-  rect(width/2,width/8,5,30)
-  
+
+  if(buttonPressed3){
   if (rotVar % 360 <= 60) {
     result = Breakfast[0]+ '(Red)';
-    image(img[0], 40, 10, 110, 110);
+    image(brk[0], 40, 10, 110, 110);
   } else if (rotVar % 360 > 60 && rotVar % 360 <= 120) {
     result = Breakfast[1]+ '(Yellow)';
-    image(img[1], 40, 10, 110, 110);
+    image(brk[1], 40, 10, 110, 110);
   } else if (rotVar % 360 > 120 && rotVar % 360 <= 180) {
     result = Breakfast[2]+ '(Green)';
-    image(img[2], 40, 10, 110, 110);
+    image(brk[2], 40, 10, 110, 110);
   } else if (rotVar % 360 > 180 && rotVar % 360 <= 240) {
     result = Breakfast[3]+ '(Blue)';
-    image(img[3], 40, 10, 110, 110);
+    image(brk[3], 40, 10, 110, 110);
   } else if (rotVar % 360 > 240 && rotVar % 360 <= 300) {
     result = Breakfast[4]+ '(Purple)';
-    image(img[4], 40, 10, 110, 110);
+    image(brk[4], 40, 10, 110, 110);
   } else if (rotVar % 360 > 300 && rotVar % 360 <= 360) {
     result = Breakfast[5] + '(Violet)';
-    image(img[5], 40, 10, 110, 110);
+    image(brk[5], 40, 10, 110, 110);
   }
-  console.log(result);
-  
+      console.log(result);
+  }
 
-  
-  
+
+
+
+
 }
 
-function buttonPressed() {
+function buttonPressed1() {
   spinnerTime = int(random(2500,5000));
   frameCheck = millis() + spinnerTime;
   var myVar = setInterval(moveDial, 25);
   setTimeout(function(){ clearInterval(myVar); }, spinnerTime);
 }
 
+function buttonPressed2() {
+  spinnerTime = int(random(2500,5000));
+  frameCheck = millis() + spinnerTime;
+  var myVar = setInterval(moveDial, 25);
+  setTimeout(function(){ clearInterval(myVar); }, spinnerTime);
+}
+
+function buttonPressed3() {
+  spinnerTime = int(random(2500,5000));
+  frameCheck = millis() + spinnerTime;
+  var myVar = setInterval(moveDial, 25);
+  setTimeout(function(){ clearInterval(myVar); }, spinnerTime);
+}
+
+function buttonPressed4() {
+  spinnerTime = int(random(2500,5000));
+  frameCheck = millis();
+  var myVar = setInterval(moveDial, 25);
+  setTimeout(function(){ clearInterval(myVar); }, spinnerTime);
+}
+
+
 function moveDial() {
-  if (millis() < (frameCheck*0.25)) {
+  if (millis() < (frameCheck+(spinnerTime*0.25))) {
     rotVar = rotVar + 15;
-  } else if (millis() < (frameCheck*0.5)) {
+  } else if (millis() < (frameCheck+(spinnerTime*0.50))) {
     rotVar = rotVar + 10;
-  } else if (millis() < (frameCheck*0.75)) {
+  } else if (millis() < (frameCheck+(spinnerTime*0.75))) {
     rotVar = rotVar + 5;
-  } else if (millis() < (frameCheck)) {
+  } else if (millis() < (frameCheck+(spinnerTime))) {
     rotVar = rotVar + 2.5;
   }
   console.log(rotVar);
-}
 
+      if (rotVar > 360){
+      rotVar = rotVar - 360;
+    }
+}
